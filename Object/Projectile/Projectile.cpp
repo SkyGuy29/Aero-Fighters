da592e3f8d55ab792f3ad960765329b18949be5b
Matthew Reustle <mrmonkey997@gmail.com>
2024-02-02 14:19:19 -0600

    Added more files to projectile folder, nothing in them yet.

--- Object/Projectile/Projectile.cpp
+++ Object/Projectile/Projectile.cpp
@@ -2,4 +2,5 @@
 
 Projectile::Projectile()
 {
+	 
 }
---
+++ Object/Projectile/StraightProjectile/StraightProjectile.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "../Projectile.h"
+
+class StraightProjectile : public Projectile
+{
+public:
+	StraightProjectile(int, int);
+	~StraightProjectile();
+protected:
+	
+};
