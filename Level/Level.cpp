311534d8107e2a56f6bc7672fb3bc6525e01a840
idk-v1 <benhamilton.jpg@gmail.com>
2024-04-16 14:02:44 -0500

    Among us amongs again and press tab for speed boost

--- Level/Level.cpp
+++ Level/Level.cpp
@@ -443,11 +443,13 @@ void Level::update(sf::Vector2u winSize)
 	{
 		if (backgroundDist <= 0)
 		{
+			frontbackgroundDist -= backgroundSpeed;
 			backgroundDist = 0;
+			rect.top = frontbackgroundDist;
 			frontbackground.setTextureRect(rect);
 		}
-		//else
-			//frontbackground.setPosition(0, -backgroundDist);
+		else
+			frontbackground.setPosition(0, -backgroundDist);
 	}
 	rect.top = backgroundDist;
 	background.setTextureRect(rect);
@@ -579,10 +581,10 @@ void Level::statesUpdate(sf::Vector2u winSize)
 
 void Level::japanUpdate(sf::Vector2u winSize)
 {
-	//if (backgroundDist <= 0 || !(p[1]->getTime()))
-		//backgroundSpeed = 0;
-	//else
-		//backgroundSpeed = 1;
+	if (backgroundDist <= 0 || !(p[1]->getTime()))
+		backgroundSpeed = 0;
+	else
+		backgroundSpeed = 1;
 }
 
 void Level::swedenUpdate(sf::Vector2u winSize)
