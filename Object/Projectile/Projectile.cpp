666c17fe851fb16e6110147ac369708abbf834f0
wynautwobbuffet <mrmonkey997@gmail.com>
2024-05-06 14:20:13 -0500

    In the middle of cleaning up projectile

--- Object/Projectile/Projectile.cpp
+++ Object/Projectile/Projectile.cpp
@@ -52,10 +52,11 @@ Projectile::Projectile(float posX, float posY, sf::Vector2f vel,
 }
 
 //The bulk of projectiles, all it does is move and check when to die.
-void Projectile::update(sf::Vector2u winSize, std::vector<Object*>* objects, bool time)
+void Projectile::update(sf::Vector2u winSize, std::vector<Object*>* objects,
+bool time)
 {
 	nextFrame(4);
-	if (delay)
+	if (delay) //If the delay is not up the projectile doesn't exist.
 	{
 		size = sf::Vector2f(0, 0);
 		delay--;
@@ -63,7 +64,7 @@ void Projectile::update(sf::Vector2u winSize, std::vector<Object*>* objects, boo
 			size = tempSize;
 	}
 
-	if (id == 3)
+	if (id == 3) //Changes the size for the Japan 2 bomb
 	{
 		setSize(sf::Vector2f(size.x += 15, size.y += 15));
 		sprite.setSize(sf::Vector2f(size.x += 15, size.y += 15));
@@ -92,6 +93,7 @@ void Projectile::update(sf::Vector2u winSize, std::vector<Object*>* objects, boo
 				|| objects->at(i)->getType() == BOSS)
 				&& this->intersect(objects->at(i))))
 		{
+			//If a player projectile interacts with an enemy.
 			del = true;
 		}
 		else if (type == ENEMY_PROJECTILE
@@ -99,6 +101,7 @@ void Projectile::update(sf::Vector2u winSize, std::vector<Object*>* objects, boo
 			&& objects->at(i)->getType() == PLAYER_PROJECTILE))
 			&& this->intersect(objects->at(i)))
 		{
+			//If an enemy projectile interacts with something that kills it.
 			if (id == 6)
 				spriteNum = 14;
 			else
@@ -108,8 +111,10 @@ void Projectile::update(sf::Vector2u winSize, std::vector<Object*>* objects, boo
 			|| objects->at(i)->getType() == LAND)
 			&& objects->at(i)->shouldDelete() == false)
 		{
+		//Used for tracking projectiles
 			newEnemyDistance = sqrt((objects->at(i)->getPos().x - pos.x)
-				* (objects->at(i)->getPos().x - pos.x) + (objects->at(i)->getPos().y - pos.y)
+				* (objects->at(i)->getPos().x - pos.x) 
+				+ (objects->at(i)->getPos().y - pos.y)
 				* (objects->at(i)->getPos().y - pos.y));
 			if (closestEnemyDistance > newEnemyDistance)
 			{
@@ -133,6 +138,7 @@ void Projectile::update(sf::Vector2u winSize, std::vector<Object*>* objects, boo
 		angle = 16 - angle;
 		if (angle > 15)
 			angle -= 16;
+		//The math works, trust me.
 		setOrientation(angle);
 		sprite.setScale(1, 1);
 		if (orientation > 8)
