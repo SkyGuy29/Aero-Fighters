da06500879a2339616f92ae32ad4bcf903db2371
KingZhara <waroperationplannedresponse@protonmail.com>
2024-08-31 17:51:08 -0500

    Added Enemy base

--- src/Object/Enemy/Enemy.cpp
+++ src/Object/Enemy/Enemy.cpp
@@ -27,48 +27,69 @@ void Enemy::enemyUpdate(sf::Vector2f winSize, std::vector<Object*>* objects)
 		del = true;
 
 	//Go to spawn place
+	// If the enemy has not yet entered the screen,
+	// Knows the backgrounds position,
+	// and the background is showing the enemies spawn position
 	if (!entered && backgroundDist != nullptr && *backgroundDist <= startMark)
 	{
+		// Spawn the enemy by setting its position and velocity
 		pos = spawnPos;
 		vel = spawnVel;
 	}
 
 	//Am I being shot?
+	// For every entity
 	for (int i = 0; i < objects->size(); i++)
 	{
+		// If the entity is a players projectile, is colliding with me, is not hidden, and is active
 		if (objects->at(i)->getType() == PLAYER_PROJECTILE 
 			&& this->intersect(objects->at(i)) && type != HIDDEN
 			&& entered)
 		{
+			// Decrement my health
 			health--;
 		}
 	}
 
 
 	// Generates Collectable items when an enemy dies, presumably dropped items. - ricky
+	// If i'm dead, not scheduled to be deleted, and not hidden
 	if (health <= 0 && del == false && type != HIDDEN)
 	{
+		// Drop powerups based on my specific type
 		switch (id)
 		{
+			// If i'm a boss
 		case 1:
+			// If i'm an Air Boss
 			if (type == AIR)
 				objects->push_back(new Collectable(1, pos, backgroundSpeed));
 			del = true;
 			break;
+
+		// If i'm a boss child entity
 		case 2:
+			// If i'm a land boss child entity
 			if(type == LAND)
 				objects->push_back(new Collectable(1, pos, backgroundSpeed));
 			del = true;
 			break;
+
+		//If i'm an explosion
 		case 8:
+			// If i'm a land explosion
 			if(type == LAND)
 				objects->push_back(new Collectable(0, pos, backgroundSpeed));
 			del = true;
 			break;
+
+		// Otherwise do nothing
 		default:
 			del = true;
 			break;
 		}
+
+		// Then spawn an explosion on my position
 		objects->push_back(new Explosion(pos, 0));
 	}
 }
--- src/Object/Enemy/Enemy.h
+++ src/Object/Enemy/Enemy.h
@@ -8,14 +8,23 @@
 class Enemy : public Object
 {
 public:
+	// Constructor
 	Enemy(short, bool, sf::Vector2f, sf::Vector2f);
 
+	// Returns the enemies health
 	int getHealth() const;
 
+	// Enemy ticking function
 	void enemyUpdate(sf::Vector2f winSize, std::vector<Object*>* objects);
 protected:
-	int health, startMark;
+	// Enemies health
+	int health;
+
+	// Starting Y position
+	int startMark;
+	// Has entered frame
 	bool entered = false;
+	// never used in base class
 	bool left;
 
 	sf::Vector2f spawnPos, spawnVel;
