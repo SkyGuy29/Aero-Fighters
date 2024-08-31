6d7f9204420c9f4c1b2fd3de6600eb896dc666a0
KingZhara <waroperationplannedresponse@protonmail.com>
2024-08-31 17:50:05 -0500

    Added collidable interface

--- src/Entities/Interfaces/ICollidable.h
+++ src/Entities/Interfaces/ICollidable.h
@@ -1,5 +1,17 @@
 #pragma once
+
+#include "../Entity.h"
+
 class ICollidable
 {
+public:
+	virtual inline const bool CollidesWith(Entity& other) noexcept = 0;
+private:
+	struct Vec2UC
+	{
+		unsigned char x, y;
+	};
+
+	Vec2UC bounds;
 };
 
