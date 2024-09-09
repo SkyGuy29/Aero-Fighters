70a2ba4e7b96279ab4ca6f8ad2d0598bfbd99136
Ninjune <enderknight537@gmail.com>
2024-09-09 11:16:36 -0500

    n

--- src/Entities/Interfaces/ICollidable.h
+++ src/Entities/Interfaces/ICollidable.h
@@ -1,11 +1,10 @@
 #pragma once
-
 #include "../Entity.h"
 
 class ICollidable
 {
 public:
-	virtual inline const bool CollidesWith(Entity& other) noexcept = 0;
+	virtual const bool CollidesWith(Entity& other) noexcept = 0;
 
 protected:
 	struct Vec2UC
