344eb2bf1ed2bc82aed838a2e29e1f6f14d7c297
Artzombie255 <artzombie255@gmail.com>
2024-09-19 12:00:35 -0500

    added cutscene base

---
+++ Cutscene.cpp
@@ -0,0 +1,1 @@
+#include "Cutscene.h"
---
+++ Cutscene.h
@@ -0,0 +1,13 @@
+#pragma once
+#include <queue>
+#include <SFML/Graphics.hpp>
+#include "VisualElement.h"
+
+class Cutscene
+{
+public:
+private:
+	std::queue<VisualElement<sf::Sprite>> spriteElements;
+	std::queue<VisualElement<sf::String>> stringElements;
+};
+
