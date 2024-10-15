fe26e5c86691a1c2ba19bed728b83d1d4bbad8d0
SkyGuy CACC <25clm04@CC134646X142>
2024-10-15 11:34:10 -0500

    VideoCutscene class is done! ish. its done for what we planned it to do.

--- src/Game/Game.h
+++ src/Game/Game.h
@@ -63,29 +63,112 @@ private:
 };
 
 
-enum class cutsceneID
+//used to determine what cutscene is used AND holds the value of the last frame for that respective cutscene
+enum cutsceneID
 {
-	START,
-	OSARU,
-	PANDORA,
-	KEATON,
-	KEATH,
-	STATES,
-	HEIN,
-	MAO,
-	JAPAN,
-	KOHFUL,
-	TEEBEE,
-	SWEDEN,
-	VILLIAM,
-	WHITE,
-	ENGLAND
+	START = 3200,
+	OSARU = 536,
+	PANDORA = 1991,
+	KEATON = 645,
+	KEATH = 567,
+	STATES = 705,
+	HEIN = 843,
+	MAO = 568,
+	JAPAN = 555,
+	KOHFUL = 746,
+	TEEBEE = 633,
+	SWEDEN = 661,
+	VILLIAM = 680,
+	WHITE = 731,
+	ENGLAND = 846
 };
 
 
+/// <summary>
+/// holds the data needed for a video cutscene (ones with complex animations that we could not recreate) and provides tools to handle said cutscenes.
+/// </summary>
 class VideoCutscene
 {
 public:
+
+	//draws the VideoCutscene to the passed in RenderWindow
+	//returns false when the cutscene is over, when the index is on the last frame
+	bool draw(sf::RenderWindow& window, cutsceneID id = cutsceneID::START)
+	{
+		loadImage(id);
+		window.draw(frame);
+		return index < lastFrame;
+	}
+
+	//sets the video back to the beginning.
+	void resetVideo() { index = 0; }
+
+	//sets the value of the last frame
+	void setLastFrame(cutsceneID id = cutsceneID::START) { lastFrame = id; }
+
+	//things needed to determine the VideoCutscene:
+	//if the boss was killed before the time ran out		bool, is this in Game or Level?
+	//if it was, which country are we and which				4 options stored in Game
+	//players are playing.									3 options stored in Game (p1 p2 or both)
+	//if not, which final boss it was						2 options, is this in Game or Level?
+	cutsceneID getID(bool bossKilled, bool pandoraWasBoss, int player, Countries country)
+	{
+		if (bossKilled)
+			switch (country)
+			{
+			case Countries::STATES:
+				switch (player)
+				{
+				case 1:
+					return cutsceneID::KEATON;
+				case 2:
+					return cutsceneID::KEATH;
+				case 3:
+					return cutsceneID::STATES;
+				}
+			case Countries::JAPAN:
+				switch (player)
+				{
+				case 1:
+					return cutsceneID::HEIN;
+				case 2:
+					return cutsceneID::MAO;
+				case 3:
+					return cutsceneID::JAPAN;
+				}
+			case Countries::SWEDEN:
+				switch (player)
+				{
+				case 1:
+					return cutsceneID::KOHFUL;
+				case 2:
+					return cutsceneID::TEEBEE;
+				case 3:
+					return cutsceneID::SWEDEN;
+				}
+			case Countries::ENGLAND:
+				switch (player)
+				{
+				case 1:
+					return cutsceneID::VILLIAM;
+				case 2:
+					return cutsceneID::WHITE;
+				case 3:
+					return cutsceneID::ENGLAND;
+				}
+			}
+		else
+			if (pandoraWasBoss)
+				return cutsceneID::PANDORA;
+			else
+				return cutsceneID::OSARU;
+	}
+
+private:
+	unsigned short lastFrame, index;
+	sf::Texture image; //i moved image to here, if we are only loading one at a time this makes it easy - Christian
+	sf::Sprite frame;
+
 	void loadImage(cutsceneID id)
 	{
 		//the while loop keeps increasing the index variable until it finds a successful load.
@@ -138,88 +221,6 @@ public:
 		}
 		frame.setTexture(image);
 	}
-
-	//returns false when the cutscene is over, when the index is on the last frame
-	bool draw(sf::RenderWindow& window)
-	{
-		loadImage(cutsceneID::START);
-		window.draw(frame);
-		return index < size;
-	}
-
-	void resetIndex() { index = 0; }
-
-	//DO NOT USE THIS CLASS UNTIL THIS FUNCTION WORKS
-	void initSize(cutsceneID)
-	{
-		//based on the cutsceneID, check the frame files to see what the last one is.
-		//WHILE THE LAST FRAME MAY NOT REPRESENT THE VIDEO SIZE, THAT IS HOW THE SIZE VARIABLE WILL BE USED.
-		//i have no idea how to code this other than to use sf::Texture.loadFromFile
-		//which is obviously not okay at all
-	}
-
-	//things needed to determine the VideoCutscene:
-	//if the boss was killed before the time ran out		bool, is this in Game or Level?
-	//if it was, which country are we and which				4 options stored in Game
-	//players are playing.									3 options stored in Game (p1 p2 or both)
-	//if not, which final boss it was						2 options, is this in Game or Level?
-	cutsceneID getID(bool bossKilled, bool pandoraWasBoss, int player, int country)
-	{
-		if (bossKilled)
-			switch (country)
-			{
-			case STATES:
-				switch (player)
-				{
-				case 1:
-					return cutsceneID::KEATON;
-				case 2:
-					return cutsceneID::KEATH;
-				case 3:
-					return cutsceneID::STATES;
-				}
-			case JAPAN:
-				switch (player)
-				{
-				case 1:
-					return cutsceneID::HEIN;
-				case 2:
-					return cutsceneID::MAO;
-				case 3:
-					return cutsceneID::JAPAN;
-				}
-			case SWEDEN:
-				switch (player)
-				{
-				case 1:
-					return cutsceneID::KOHFUL;
-				case 2:
-					return cutsceneID::TEEBEE;
-				case 3:
-					return cutsceneID::SWEDEN;
-				}
-			case ENGLAND:
-				switch (player)
-				{
-				case 1:
-					return cutsceneID::VILLIAM;
-				case 2:
-					return cutsceneID::WHITE;
-				case 3:
-					return cutsceneID::ENGLAND;
-				}
-			}
-		else
-			if (pandoraWasBoss)
-				return cutsceneID::PANDORA;
-			else
-				return cutsceneID::OSARU;
-	}
-
-private:
-	unsigned short size, index; //should we rename size to lastFrame? might be more clear that way
-	sf::Texture image; //i moved image to here, if we are only loading one at a time this makes it easy - Christian
-	sf::Sprite frame;
 };
 
 
