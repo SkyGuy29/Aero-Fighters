bb4b8a84cd90c2bc373a5b1f0a78ffb75e700128
artzombie255 <artzombie255@gmail.com>
2024-10-21 12:04:55 -0500

    added missle copypasta back in

--- src/Level/Level.cpp
+++ src/Level/Level.cpp
@@ -115,6 +115,39 @@ void Level::load(sf::Vector2f winSize, const short country,
 	/*objects.push_back(new Air(0, true, &backgroundDist, 0, winSize,
 		&objects, sf::Vector2f(winSize.x * 0.5f,
 	winSize.y * 0.5f), sf::Vector2f(0, 0), levelEditor));*/
+
+	/*
+		The enemy knows where it is at all times.
+		It knows this because it knows where it isn't.
+		By subtracting where it is from where it isn't,
+		or where it isn't from where it is (whichever is greater),
+		it obtains a difference, or deviation.
+		The guidance subsystem uses deviations to generate
+		corrective commands to drive the missile from a position
+		where it is to a position where it isn't,
+		and arriving at a position where it wasn't, it now is.
+		Consequently, the position where it is,
+		is now the position that it wasn't,
+		and it follows that the position that it was,
+		is now the position that it isn't.
+		In the event that the position that it is in is not
+		the position that it wasn't, the system has acquired a variation,
+		the variation being the difference between where the enemy is,
+		and where it wasn't.
+		If variation is considered to be a significant factor,
+		it too may be corrected by the GEA.
+		However, the enemy must also know where it was.
+		The enemy guidance computer scenario works as follows.
+		Because a variation has modified some of the information
+		the enemy has obtained, it is not sure just where it is.
+		However, it is sure where it isn't, within reason,
+		and it knows where it was.
+		It now subtracts where it should be from where it wasn't,
+		or vice-versa, and by differentiating this from the
+		algebraic sum of where it shouldn't be, and where it was,
+		it is able to obtain the deviation and its variation,
+		which is called error.
+	*/
 }
 
 
@@ -181,10 +214,10 @@ bool Level::update(const sf::Vector2f winSize)
 	// Drawing order
 	EntityManagementInterface::tick(window, currentTick);
 	
-	updatePlayers();
+	/*updatePlayers();
 	p[0]->updateBgSpeed(&backgroundSpeed);
 	p[1]->updateBgSpeed(&backgroundSpeed);
-	
+	*/
 
 	// checking the back of the vector first is needed,
 	// so deleting doesn't shift everything down and mess up the for loop
