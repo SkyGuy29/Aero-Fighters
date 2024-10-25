b73e7f2f3747543e8a3fd7689ea9d7b13ae7664d
KingZhara <waroperationplannedresponse@protonmail.com>
2024-10-25 11:37:43 -0500

    Child Abuse Awareness

--- src/Utility/EntityData/ProjectilePrototype.h
+++ src/Utility/EntityData/ProjectilePrototype.h
@@ -6,15 +6,15 @@ struct ProjectilePrototype : EntityPrototype
 {
 	enum class Owner
 	{
-		Enemy,
-		Player1,
-		Player2
+		ENEMY,
+		PLAYER1,
+		PLAYER2
 	};
 
-	ProjectilePrototype(sf::Vector2f spawnPos, sf::Vector2f spawnVelocity, EntityID id, unsigned int tickOffset, uint8_t flags/*, Owner owner*/)
-		: EntityPrototype(spawnPos, spawnVelocity, id, tickOffset, 0), FLAGS(flags)/*, OWNER(owner)*/ {}
+	ProjectilePrototype(sf::Vector2f spawnPos, sf::Vector2f spawnVelocity, EntityID id, unsigned int tickOffset, uint8_t flags, Owner owner = Owner::ENEMY)
+		: EntityPrototype(spawnPos, spawnVelocity, id, tickOffset, 0), FLAGS(flags), OWNER(owner) {}
 	
 	// 0/0/0/0/0/0/0/set velocity on start to go to nearest player
 	const uint8_t FLAGS;
-	//const Owner OWNER;
+	const Owner OWNER;
 };
