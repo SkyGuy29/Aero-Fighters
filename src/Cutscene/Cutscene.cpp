dcf8b38d03c10296a8b5dcbc2570c9e1d513e6c6
Requlix <91223411+Requlix@users.noreply.github.com>
2024-11-11 11:39:04 -0600

    Update Cutscene.cpp

--- src/Cutscene/Cutscene.cpp
+++ src/Cutscene/Cutscene.cpp
@@ -4,8 +4,8 @@
 Cutscene::Cutscene()
 {
 	std::ifstream englandFile;
-	englandFile.open("res/Cutscenes/Dialog/englandDialog.txt");
-	if(englandFile.is_open())
+	englandFile.open("res/Dialog/englandDialog.txt");
+	if (englandFile.is_open())
 		for (int l = 0; l < 9; l++)
 			for (int p = 0; p < 3; p++)
 				for (int d = 0; d < 6; d++)
@@ -13,7 +13,7 @@ Cutscene::Cutscene()
 	englandFile.close();
 
 	std::ifstream japanFile;
-	japanFile.open("res/Cutscenes/Dialog/japanDialog.txt");
+	japanFile.open("res/Dialog/japanDialog.txt");
 	if (japanFile.is_open())
 		for (int l = 0; l < 9; l++)
 			for (int p = 0; p < 3; p++)
@@ -22,7 +22,7 @@ Cutscene::Cutscene()
 	japanFile.close();
 
 	std::ifstream statesFile;
-	statesFile.open("res/Cutscenes/Dialog/statesDialog.txt");
+	statesFile.open("res/Dialog/statesDialog.txt");
 	if (statesFile.is_open())
 		for (int l = 0; l < 9; l++)
 			for (int p = 0; p < 3; p++)
@@ -31,7 +31,7 @@ Cutscene::Cutscene()
 	statesFile.close();
 
 	std::ifstream swedenFile;
-	swedenFile.open("res/Cutscenes/Dialog/swedenDialog.txt");
+	swedenFile.open("res/Dialog/swedenDialog.txt");
 	if (swedenFile.is_open())
 		for (int l = 0; l < 9; l++)
 			for (int p = 0; p < 3; p++)
@@ -45,7 +45,7 @@ Cutscene::Cutscene()
 		textDia[i].setFont(font);
 		textDia[i].setCharacterSize(8);
 		textDia[i].setFillColor(sf::Color::White);
-		textDia[i].setScale(textDia[i].getScale().x, textDia[i].getScale().y * 3./2);
+		textDia[i].setScale(textDia[i].getScale().x, textDia[i].getScale().y * 3. / 2);
 	}
 	textDia[0].setPosition(23, 237);
 	textDia[3].setPosition(23, 237);
@@ -59,20 +59,23 @@ Cutscene::Cutscene()
 	lvlBeatTxt.loadFromFile("res/Misc/lvlBeat.png");
 	lvlBeatSprite.setTexture(lvlBeatTxt);
 
-	playersText[England][0].loadFromFile("res/Cutscenes/Dialog/villiam.png");
-	playersText[England][1].loadFromFile("res/Cutscenes/Dialog/lord white.png");
-	playersText[Japan][0].loadFromFile("res/Cutscenes/Dialog/hein.png");
-	playersText[Japan][1].loadFromFile("res/Cutscenes/Dialog/mao.png");
-	playersText[States][0].loadFromFile("res/Cutscenes/Dialog/keaton.png");
-	playersText[States][1].loadFromFile("res/Cutscenes/Dialog/keath.png");
-	playersText[Sweden][0].loadFromFile("res/Cutscenes/Dialog/kohful.png");
-	playersText[Sweden][1].loadFromFile("res/Cutscenes/Dialog/tee-bee.png");
+	playersText[England][0].loadFromFile("res/Dialog/villiam.png");
+	playersText[England][1].loadFromFile("res/Dialog/lord white.png");
+	playersText[Japan][0].loadFromFile("res/Dialog/hein.png");
+	playersText[Japan][1].loadFromFile("res/Dialog/mao.png");
+	playersText[States][0].loadFromFile("res/Dialog/keaton.png");
+	playersText[States][1].loadFromFile("res/Dialog/keath.png");
+	playersText[Sweden][0].loadFromFile("res/Dialog/kohful.png");
+	playersText[Sweden][1].loadFromFile("res/Dialog/tee-bee.png");
 
 	playersSprite[0].setPosition(24, 112);
 	playersSprite[1].setPosition(120, 112);
 
-	targetText[0].loadFromFile("res/Cutscenes/Dialog/target1.png");
-	targetText[1].loadFromFile("res/Cutscenes/Dialog/target2.png");
+	targetText[0].loadFromFile("res/Dialog/target1.png");
+	targetText[1].loadFromFile("res/Dialog/target2.png");
+
+	for (int i = 0; i < 17; i++)
+		explosionTxt[i].loadFromFile("res/Dialog/Explosion" + std::to_string(i) + ".png");
 }
 
 Cutscene::~Cutscene()
@@ -84,7 +87,7 @@ void Cutscene::draw(int country, int level, int player, sf::RenderWindow& window
 {
 	playersSprite[0].setTexture(playersText[country][0]);
 	playersSprite[1].setTexture(playersText[country][1]);
-	if(!lvlBeat[0] && !lvlBeat[1] && !lvlBeat[2] && !lvlBeat[3] && !lvlBeat[4] && !lvlBeat[5] && !lvlBeat[6])
+	if (!lvlBeat[0] && !lvlBeat[1] && !lvlBeat[2] && !lvlBeat[3] && !lvlBeat[4] && !lvlBeat[5] && !lvlBeat[6])
 		for (int i = 0; i < 6; i++)
 			textDia[i].setString(dialog[country][8][player][i]);
 	else
@@ -128,7 +131,7 @@ void Cutscene::draw(int country, int level, int player, sf::RenderWindow& window
 			lvlBeatSprite.setPosition(108, 12);
 			window.draw(lvlBeatSprite);
 		}
-		
+
 		switch (player)
 		{
 		case 0:
@@ -143,10 +146,35 @@ void Cutscene::draw(int country, int level, int player, sf::RenderWindow& window
 			break;
 		}
 
+		switch (lastLvl)
+		{
+		case England:
+			explosion.setPosition(82, 16);
+			break;
+		case Israel:
+			explosion.setPosition(108, 32);
+			break;
+		case Japan:
+			explosion.setPosition(168, 24);
+			break;
+		case Meddit:
+			explosion.setPosition(90, 34);
+			break;
+		case Russia:
+			explosion.setPosition(112, 8);
+			break;
+		case States:
+			explosion.setPosition(40, 24);
+			break;
+		case Sweden:
+			explosion.setPosition(92, -4);
+			break;
+		}
+
 		switch (level)
 		{
 		case England:
-			targetSprite.setPosition(98,32);
+			targetSprite.setPosition(98, 32);
 			break;
 		case Israel:
 			targetSprite.setPosition(124, 48);
@@ -168,9 +196,20 @@ void Cutscene::draw(int country, int level, int player, sf::RenderWindow& window
 			break;
 		}
 
+
 		int temp = timer.getElapsedTime().asSeconds() * 3;
-		targetSprite.setTexture(targetText[temp%2]);
+		targetSprite.setTexture(targetText[temp % 2]);
 		window.draw(targetSprite);
+
+		double otherTemp = timer.getElapsedTime().asSeconds();
+
+		if (otherTemp < 1.)
+		{
+			window.draw(explosion);
+			for (int i = 0; i <= 17; i++)
+				if ((i - 1)/17. < otherTemp && i/17. < otherTemp)
+					explosion.setTexture(explosionTxt[i-1]);
+		}
 
 		if (timer.getElapsedTime().asSeconds() < 2.5)
 		{
@@ -194,4 +233,5 @@ void Cutscene::startTimer()
 void Cutscene::levelBeat(int level)
 {
 	lvlBeat[level] = true;
+	lastLvl = level;
 }
