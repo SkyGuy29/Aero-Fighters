7d044b8d5ffdb4c66df75333e2de07107f750bb4
artzombie255 <artzombie255@gmail.com>
2024-11-18 12:03:29 -0600

    quick fix

--- src/Entities/PowerUp/PowerUp.cpp
+++ src/Entities/PowerUp/PowerUp.cpp
@@ -10,12 +10,12 @@ PowerUp::PowerUp(sf::Vector2f spawnPos, EntityID id)
 void PowerUp::interceptBarrier()
 {
     sf::Vector2f pos = getPosition();
-    if (pos.x < 0 || pos.x > getWinSize().x)
+    if (pos.x < 0 || pos.x > winSize.x)
     {
         (vel.x *= -1);
         bounces++;
     }
-    if (pos.y < 0 || pos.y > getWinSize().y)
+    if (pos.y < 0 || pos.y > winSize.y)
     {
         (vel.y *= -1);
         bounces++;
