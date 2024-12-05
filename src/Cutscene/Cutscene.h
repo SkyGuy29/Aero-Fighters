e0312bb249d0f3eff24a08deaf0d8b2e609be723
Alfred Ward <25ARW05@stu.cpsk12.org>
2024-12-05 11:42:02 -0600

    more cutscene comments

--- src/Cutscene/Cutscene.h
+++ src/Cutscene/Cutscene.h
@@ -10,6 +10,7 @@ class Cutscene
 public:
 	Cutscene();
 	~Cutscene();
+	//attacking countries
 	enum Map
 	{
 		England = 0,
@@ -22,6 +23,7 @@ public:
 		Space
 	};
 
+	//useable countries
 	enum Country
 	{
 		STATES,
@@ -30,28 +32,41 @@ public:
 		ENGLAND
 	};
 
+	//draws cutscenes
 	void draw(int, int, int, sf::RenderWindow&);
+	//go check Cutscene.cpp for comments on this stuff cys im lazy
 	void startTimer();
 	void levelBeat(int);
 	bool isDone();
 private:
 
+	//indications of which levels have been beat so far
 	bool lvlBeat[7] = { false,false,false,false,false,false,false };
 
+	//needed it to be set to something literally can be any number other than 0-8
 	int lastLvl = 365;
 
+	//all dialog for cutscenes organized
 	std::string dialog[4][9][3][6];
+	//font
 	sf::Font font;
+	//the current cutscenes text for dialog
 	sf::Text textDia[6];
+	//the current countries players being used
 	sf::Texture playersText[4][2];
 	sf::Sprite playersSprite[2];
+	//indication for a beat level
 	sf::Texture lvlBeatTxt;
 	sf::Sprite lvlBeatSprite;
+	//background map thing where stuff is
 	sf::Sprite sprMap;
 	sf::Texture txtMap;
+	//indicater for next level
 	sf::Texture targetText[2];
 	sf::Sprite targetSprite;
+	//timer for timing things
 	sf::Clock timer;
+	//WE BRING THE BOOM THATS WHAT WE DO
 	sf::Texture explosionTxt[16];
 	sf::Sprite explosion;
 
